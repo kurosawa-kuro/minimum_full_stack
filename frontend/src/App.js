5f254b72b759883130b2c6239d9bdc6f466ef04e
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-04-23 11:26:55 +0900

    WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,62 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+// import scss
+import "./style.scss";
+
+// Layout
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   const [items, setItems] = useState([]);
@@ -13,10 +69,10 @@ function App() {
   }, []);
 
   return (
-    <div className="container">
-      {items.map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
